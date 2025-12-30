b1d181b094ed86e70f9160213135c719cb18738b
devwilson <ohioleomole@gmail.com>
2025-12-30 18:16:52 +0000

    implement retrieving events

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -1,6 +1,6 @@
 import { redis } from "@/lib/redis";
-import { parse } from "date-fns";
 import { getDate } from "./utils/getDate";
+import { parse } from "date-fns";
 
 type AnalyticsArgs = {
   retention?: number;
@@ -13,20 +13,59 @@ type TrackOptions = {
 export class Analytics {
   private retention: number = 60 * 60 * 24 * 7; // 7 days
 
-  constructor(opts?: AnalyticsArgs) {
-    if (opts?.retention) this.retention = opts.retention;
+  constructor(options?: AnalyticsArgs) {
+    if (options?.retention) this.retention = options.retention;
   }
 
-  async track(namespace: string, event: object = {}, opts?: TrackOptions) {
+  async track(namespace: string, event: object = {}, options?: TrackOptions) {
     let key = `analytics::${namespace}`;
 
-    if (!opts?.persist) {
+    if (!options?.persist) {
       key += `::${getDate()}`;
     }
 
     // db call to persist this event
     await redis.hincrby(key, JSON.stringify(event), 1);
-    if (!opts?.persist) await redis.expire(key, this.retention);
+    if (!options?.persist) await redis.expire(key, this.retention);
+  }
+
+  async retrieveDays(namespace: string, numberOfDays: number) {
+    type AnalyticsPromise = ReturnType<typeof analytics.retrieve>;
+    const promises: AnalyticsPromise[] = [];
+
+    for (let i = 0; i < numberOfDays; i++) {
+      const formattedDate = getDate(i);
+      const promise = analytics.retrieve(namespace, formattedDate);
+      promises.push(promise);
+    }
+
+    const fetched = await Promise.all(promises);
+
+    const data = fetched.sort((a, b) => {
+      if (
+        parse(a.date, "dd/MM/yyyy", new Date()) >
+        parse(b.date, "dd/MM/yyyy", new Date())
+      ) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+
+    return data;
+  }
+
+  async retrieve(namespace: string, date: string) {
+    const res = await redis.hgetall<Record<string, string>>(
+      `analytics::${namespace}::${date}`
+    );
+
+    return {
+      date,
+      events: Object.entries(res ?? []).map(([key, value]) => ({
+        [key]: Number(value),
+      })),
+    };
   }
 }
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,8 @@ export default async function middleware(req: NextRequest) {
     try {
       await analytics.track("pageview", {
         page: "/",
-        // country: req.destination,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        country: (req as any).geo.country,
       });
     } catch (err) {
       // fail silently to not affect request
